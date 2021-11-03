158b425d1e62fea2ea0a6a8855d57b4c00b401a8
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-11-04 02:29:30 +0900

    Add E2E testing workflow (#7)

--- tests/infra/lib/infra-stack.ts
+++ tests/infra/lib/infra-stack.ts
@@ -2,17 +2,20 @@ import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
 import * as cdk from '@aws-cdk/core';
 import { RemovalPolicy } from '@aws-cdk/core';
 
+interface InfraStackProps extends cdk.StackProps {
+  name: string
+}
+
 export class InfraStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: InfraStackProps) {
     super(scope, id, props);
 
     new Table(this, 'CSV2DynamoDBTable', {
-      tableName: `csv2dynamodb-test`,
+      tableName: props.name,
       partitionKey: {
         name: 'test',
         type: AttributeType.STRING
       },
-      timeToLiveAttribute: 'expire',
       removalPolicy: RemovalPolicy.DESTROY
     });
   }
