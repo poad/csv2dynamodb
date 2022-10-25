4737e0b24982258df49f19911c2ad513e84cb42e
Kenji Saito <ken-yo@mbr.nifty.com>
2022-10-25 10:01:30 +0900

    Bumps node modules

---
+++ update.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}" || exit
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+cargo update
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}"/tests/infra || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+yarn install && yarn upgrade && rm -rf node_modules && yarn install --production && git add node_modules -f
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CUR}" || exit
